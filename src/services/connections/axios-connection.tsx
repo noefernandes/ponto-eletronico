03196191331eede06788f9be031588aab1620ea1
noefernandes <noepessoa@outlook.com>
2024-12-05 18:16:32 -0300

    feat: adiciona nova url de conexão

--- src/services/connections/axios-connection.tsx
+++ src/services/connections/axios-connection.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api/",
+    baseURL: "https://ponto-eletronico-api.onrender.com/api/",
     headers: {
         "Content-Type": "application/json",
     },
